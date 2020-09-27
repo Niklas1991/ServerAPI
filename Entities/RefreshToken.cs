bd5e73c140e98c2dfa092ef2fab42e19762fb8fb
Niklas1991 <Niklas1991@users.noreply.github.com>
2020-09-27 10:18:29 +0200

    Removed unnecessary code

--- Entities/RefreshToken.cs
+++ Entities/RefreshToken.cs
@@ -15,9 +15,9 @@ namespace ServerAPI.Entities.Response
         public DateTime Expires { get; set; }
         public bool IsExpired => DateTime.UtcNow >= Expires;
         public DateTime Created { get; set; }
-		//public string CreatedByIp { get; set; }
+		
 		public DateTime? Revoked { get; set; }
-		//public string RevokedByIp { get; set; }
+		
 		public string ReplacedByToken { get; set; }
         public bool IsActive => Revoked == null && !IsExpired;
     }
